a951d259292d9fe924dd968ab54b44ed5faeba57
Borys <83366051+fxxsx@users.noreply.github.com>
2022-05-15 16:07:01 +0200

    :heavy_plus_sign: Update Main.py
    Added Comments, Changed the naming

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import plotly.express as px
-url = 'http://api.open-notify.org/iss-now.json'
-df = pd.read_json(url) 
-df['latitude'] = df.loc['latitude', 'iss_position']
-df['longitude'] = df.loc['longitude', 'iss_position']
+url = 'http://api.open-notify.org/iss-now.json' #create a variable
+df = pd.read_json(url) #read the data
+df['latitude'] = df.loc['latitude', 'iss_position']   #create a new column that consists of latitude and iss_position
+df['longitude'] = df.loc['longitude', 'iss_position'] #same here but this time it's longitude
 df.reset_index(inplace=True)
-df = df.drop(['index', 'message'], axis=1)
-x = px.scatter_geo(df, lat='latitude', lon='longitude')
-x.show()
+df = df.drop(['index', 'message'], axis=1) #drop the index and the message, they are not needed
+fig = px.scatter_geo(df, lat='latitude', lon='longitude')  #scatter the DataFrame
+fig.show() #show where the ISS is
